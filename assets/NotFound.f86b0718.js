124918f12e7eb70e7fcaab98339284899e4fcf79
Tatjana <tatjana@pilabs.me>
2022-12-25 11:12:56 +0100

    demo 7

---
+++ assets/NotFound.f86b0718.js
@@ -0,0 +1,1 @@
+import{F as e}from"./FooterComponent.590b3a32.js";import{e as o,f as t,i as a,g as n}from"./index.c511fc16.js";const r=n("h1",null,"Not found",-1),p={__name:"NotFound",setup(s){return(c,_)=>(o(),t("div",null,[r,a(e)]))}};export{p as default};
